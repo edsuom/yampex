ce33872699bbe0e207f97ac95af8207ccc45e66e
server <foss@edsuom.com>
2019-05-16 00:14:25 -0700

    RELEASE 0.9.3
    Since RELEASE 0.9.2
    -------------------------------------------------------------
    Finally fixed nearly unpositionable figure title by using a text box.
    
    TextBoxMaker now supports locations (including "N", "E", "S", and
    "W"), not just quadrants. Also more powerful.
    
    Made underlying Matplotlib Axes object accessible from SpecialAx for
    use when necessary, e.g., for errorbar plot (not yet supported via
    SpecialAx).
    
    Added convenience function yampex.xy.
    
    For the add_annotation function, you can just supply an int or float
    as the second argument instead of a string and the annotation will
    display the formatted value.
    
    Plotter() now accepts Nc keyword, which you can specify with N and have Nr
    calculated.
    
    Added more examples.
    
    Fixed bug where "None" was added as a legend when none had been
    defined, or the number of plot lines exceeds the number of legends
    defined.
    
    Made more tolerant of empty vectors.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ required = ['numpy', 'matplotlib', 'screeninfo']
 
 
 ### Define setup options
-kw = {'version':'0.9.2',
+kw = {'version':'0.9.3',
       'license':'Apache License (2.0)',
       'platforms':'OS Independent',
 
